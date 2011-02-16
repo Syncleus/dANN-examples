3d8474fbf7fcf4ed76132e284eaba5d429b52b7a
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2011-02-16 15:20:19 -0500

    updated run.sh to reflect newer library layout

--- script/run.sh
+++ script/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Xmx320m -classpath ../build/jar/dann_examples.jar:../../java_dann/build/jar/dann.jar:../lib/j3dcore.jar:../lib/vecmath.jar:../lib/j3dutils.jar com.syncleus.dann.examples.Main
+java -Xmx320m -classpath ./target/dann-examples-2.0-SNAPSHOT.jar:../java_dann/target/dann-core-2.0-SNAPSHOT.jar:./lib/run/java3d/j3dcore.jar:./lib/run/java3d/vecmath.jar:./lib/run/java3d/j3dutils.jar:./lib/run/log4j/log4j-1.2.15.jar:./lib/run/jaxb/jaxb2-basics-runtime-0.5.3.jar com.syncleus.dann.examples.Main
