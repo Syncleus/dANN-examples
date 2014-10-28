567a949d9eb82b4143a0328e4471e8b8cdf4fa48
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2014-10-27 23:43:11 -0400

    Removed defunct modules and files.
    DANN-4

--- script/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -Xmx320m -classpath ./target/dann-examples-2.0-SNAPSHOT.jar:../java_dann/target/dann-core-2.0-SNAPSHOT.jar:./lib/run/java3d/j3dcore.jar:./lib/run/java3d/vecmath.jar:./lib/run/java3d/j3dutils.jar:./lib/run/log4j/log4j-1.2.15.jar:./lib/run/jaxb/jaxb2-basics-runtime-0.5.3.jar com.syncleus.dann.examples.Main
